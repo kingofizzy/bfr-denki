2d8d5265c718dad013759e1a9052fb9bd1b01b8f
Hyper Network <123313247+kingofizzy@users.noreply.github.com>
2025-01-19 20:35:03 +0530

    Update welcome.py

--- RishuMusic/plugins/tools/welcome.py
+++ RishuMusic/plugins/tools/welcome.py
@@ -78,7 +78,7 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
     return pfp
 
 def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
-    background = Image.open("RishuMusic/assets/RISHUWLCM.png")
+    background = Image.open("RishuMusic/assets/wel.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
     pfp = pfp.resize((500, 500))
@@ -86,8 +86,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     font = ImageFont.truetype('RishuMusic/assets/font.ttf', size=60)
     welcome_font = ImageFont.truetype('RishuMusic/assets/font.ttf', size=60)
     
- #   draw.text((630, 230), f"USERNAME : {uname}", fill=(255, 255, 255), font=font)
-   # draw.text((630, 300), f'NAME: {user}', fill=(255, 255, 255), font=font)
+
     draw.text((630, 450), f'ID: {id}', fill=(255, 255, 255), font=font)
 
     pfp_position = (48, 88)
@@ -169,8 +168,11 @@ async def greet_new_member(_, member: ChatMemberUpdated):
 **▬▭▬▭▬▭▬▭▬▭▬▭▬▭**
 
 **☉ ɴᴀᴍᴇ ⧽** {user.mention}
+
 **☉ ɪᴅ ⧽** `{user.id}`
+
 **☉ ᴜ_ɴᴀᴍᴇ ⧽** @{user.username}
+
 **☉ ᴛᴏᴛᴀʟ ᴍᴇᴍʙᴇʀs ⧽** {count}
 
 **▬▭▬▭▬▭▬▭▬▭▬▭▬▭**
