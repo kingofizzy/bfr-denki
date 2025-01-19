093f70e32bd1ce778643faa9fe6c2a3a7a660a77
Hyper Network <123313247+kingofizzy@users.noreply.github.com>
2025-01-20 05:27:09 +0530

    Update welcome.py

--- RishuMusic/plugins/tools/welcome.py
+++ RishuMusic/plugins/tools/welcome.py
@@ -81,7 +81,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     background = Image.open("RishuMusic/assets/wel.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
-    pfp = pfp.resize((350, 350))
+    pfp = pfp.resize((340, 340))
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype('RishuMusic/assets/font.ttf', size=0)
     welcome_font = ImageFont.truetype('RishuMusic/assets/font.ttf', size=0)
